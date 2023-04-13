620bea22f707556f37d814deabd9000708792f9c
Karan Nayak <107341562+kloudsoft91@users.noreply.github.com>
2023-04-13 12:41:45 +1200

    Update BookingController.cs
    Expanded what the POST request returns and performs so that we can test it

--- WebApplication/Controllers/BookingController.cs
+++ WebApplication/Controllers/BookingController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+//using System.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using ParkingWebApplication.Data;
 using ParkingWebApplication.Models;
 
@@ -7,6 +8,16 @@ namespace ParkingWebApplication.Controllers
     [Route("api/[controller]")]
     public class BookingController : ControllerBase
     {
+        private ParkingCostModel ConvertToParkingCost(ParkingBooking model)
+        {
+            ParkingCostModel data = new ParkingCostModel();
+            data.Rego = model.Rego;
+            data.Type = model.Type;
+            data.ParkingStart = model.ParkingStart;
+            data.ParkingEnd = model.ParkingEnd;
+            return data;
+        }
+
         // GET api/booking
         [HttpGet]
         public IActionResult GetAll()
@@ -19,8 +30,44 @@ namespace ParkingWebApplication.Controllers
         [HttpPost]
         public IActionResult Post([FromBody] ParkingBooking body)
         {
-            new ParkingDBContext().Insert(body);
-            return new OkObjectResult("");
+            IKiosk kiosk;
+            DateTime startTime;
+            DateTime endTime;
+
+            ParkingCostModel model = ConvertToParkingCost(body);
+
+            if (ModelState.IsValid)
+            {
+                startTime = model.ParkingStart;
+                endTime = model.ParkingEnd;
+
+                if (model.Type == "Student")
+                {
+                    kiosk = new StudKioskWrap(startTime, endTime);
+                }
+                else if (model.Type == "Staff")
+                {
+                    kiosk = new StaffKioskWrap(startTime, endTime);
+                }
+                else
+                {
+                    kiosk = new GenKioskWrap(startTime, endTime);
+                }
+                try
+                {
+                    string parkingAmount = kiosk.FindParkingAmount().ToString();
+                    new ParkingDBContext().Insert(body);
+                    return new OkObjectResult(parkingAmount);
+                }
+                catch
+                {
+                    return new BadRequestObjectResult("Unable to book for over 24 hrs");
+                }
+            }
+            else
+            {
+                return new BadRequestObjectResult("Incorrect object passed to API");
+            }
         }
     }
 }
